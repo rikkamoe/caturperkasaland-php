683f178c1b33fca97a76f70ce6052187929d7c14
RikkaMoe <bayuwidiastika@gmail.com>
2021-06-18 12:11:05 +0800

    Update Reporting Data

--- laporan.php
+++ laporan.php
@@ -21,14 +21,17 @@ $html = '<!DOCTYPE html>
 			                	<th>No</th>
 			                    <th>Tanggal Penjualan</th>
 			                    <th>Judul Property</th>
+			                    <th>Pemilik</th>
 			                    <th>Harga</th>
 			                </tr>
 			            </thead>
 			            <tbody>';
 			            	$no = 1;
 			            	$laporansql = "
-			            		SELECT * FROM tb_property 
-			            		WHERE id_agent = '$id' AND status_property = 'Sudah Terjual' AND tanggal LIKE '$tahun%'";
+			            		SELECT * FROM tb_property
+			            		INNER JOIN tb_user
+			            		ON tb_property.id_pemilik = tb_user.id 
+			            		WHERE id_agent = '$id' AND status_property = '5' AND tanggal LIKE '$tahun%'";
 			            	$laporan = mysqli_query($conn, $laporansql);
 			            	while ($ceklaporan = mysqli_fetch_array($laporan)) 
 			            	{
@@ -37,18 +40,19 @@ $html = '<!DOCTYPE html>
 			                    <td>'.$no++.'</td>
 			                    <th>'.$ceklaporan['tanggal'].'</th>
 			                    <td>'.$ceklaporan['judul'].'</td>
+			                    <td>'.$ceklaporan['nama'].'</td>
 			                    <td>Rp. '.$ceklaporan['harga'].'</td>
 			                </tr>';
 			            	}
 			            	$totallaporansql = "
 			            		SELECT SUM(harga) AS total_laporan 
 			            		FROM tb_property
-			            		WHERE id_agent = '$id' AND status_property = 'Sudah Terjual' AND tanggal LIKE '$tahun%'";
+			            		WHERE id_agent = '$id' AND status_property = '5' AND tanggal LIKE '$tahun%'";
 			            	$totallaporan = mysqli_query($conn, $totallaporansql);
 			            	$datatotal = mysqli_fetch_array($totallaporan);
 			             	$html .='
 			             	<tr>
-			             		<td colspan="3">Jumlah Profit</td>
+			             		<td colspan="4">Jumlah Profit</td>
 			             		<td>Rp. '.$datatotal['total_laporan'].'</td>
 			             	</tr>
 			            </tbody>
